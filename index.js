b2bdc04d91627b9fa86aba3e96845650b15fad0c
Marcin Szczepanski <mszczepanski@atlassian.com>
2021-02-05 16:18:48 +1100

    Support for multiple SKUs

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 const got = require("got");
 
-const UPDATE_TEMPLATE = (name, status) =>
-  `${name} is ${status}`;
+const UPDATE_TEMPLATE = (name, status) => `${name} is ${status}`;
 const CHANGE_TEMPLATE = (name, oldStatus, status) =>
   `Update! ${name} ${oldStatus} → ${status}`;
 
 const PUSHOVER_API_URL = `https://api.pushover.net/1/messages.json`;
 
-const sku = "92176";
 const BASE_URL = "https://www.lego.com/en-au/product/";
-const RE_NAME = new RegExp(/^LEGO®\s*(?<name>.*?)\s*\|.*/);
+const RE_NAME = new RegExp(/^(LEGO®)?\s*(?<name>.*?)\s*\|.*/);
 
 async function push(token, user, title, url, message) {
   return got.post(PUSHOVER_API_URL, {
@@ -26,9 +24,25 @@ async function push(token, user, title, url, message) {
   });
 }
 
+async function getStatusForSku(sku) {
+  const productUrl = `${BASE_URL}${sku}`;
+  const page = await got(productUrl);
+
+  const $ = cheerio.load(page.body);
+  const title = $("title").text();
+  const match = title.match(RE_NAME);
+  if (!match || !match.groups) {
+    throw new Error(`Unable to match ${RE_NAME} in title ${title} for ${sku}`);
+  }
+  const name = match.groups.name;
+  const currentStatus = $(`[data-test='product-overview-availability']`).text();
+  return { name, productUrl, currentStatus };
+}
+
 async function main() {
+  const skus = process.argv[2].split(",");
   try {
-    const secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+    const secrets = JSON.parse(fs.readFileSync("secrets.json", "utf8"));
     let state = {};
     try {
       state = JSON.parse(fs.readFileSync("state.json", "utf8"));
@@ -38,23 +52,31 @@ async function main() {
       };
     }
 
-    const productUrl = `${BASE_URL}${sku}`;
-    const page = await got(productUrl);
-
-    const $ = cheerio.load(page.body);
-    const title = $("title").text();
-    const match = title.match(RE_NAME);
-    const name = match.groups.name;
-    const currentStatus = $(
-      `[data-test='product-overview-availability']`
-    ).text();
-    if (currentStatus !== state.status) {
-      console.log("Status change! " + currentStatus);
-      push(secrets.PUSHOVER_TOKEN, secrets.PUSHOVER_USER, name, productUrl, CHANGE_TEMPLATE(name, state.status, currentStatus));
-    } else if (process.argv[2] === '--send-update') {
-      push(secrets.PUSHOVER_TOKEN, secrets.PUSHOVER_USER, name, productUrl, UPDATE_TEMPLATE(name, currentStatus));
+    for (sku of skus) {
+      console.log(`Getting status for SKU ${sku}`);
+      let status = state[sku] || null;
+      const { name,  productUrl, currentStatus } = await getStatusForSku(sku);
+      if (currentStatus !== status) {
+        console.log("Status change! " + currentStatus);
+        push(
+          secrets.PUSHOVER_TOKEN,
+          secrets.PUSHOVER_USER,
+          name,
+          productUrl,
+          CHANGE_TEMPLATE(name, status, currentStatus)
+        );
+      } else if (process.argv[3] === "--send-update") {
+        push(
+          secrets.PUSHOVER_TOKEN,
+          secrets.PUSHOVER_USER,
+          name,
+          productUrl,
+          UPDATE_TEMPLATE(name, currentStatus)
+        );
+      }
+      console.log(sku, currentStatus);
+      state[sku] = { status: currentStatus };
     }
-    state.status = currentStatus;
 
     fs.writeFileSync("state.json", JSON.stringify(state), "utf8");
   } catch (err) {
